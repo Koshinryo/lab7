055ba75c86c029e7b6010b6e31abe71309a52aa6
vagrant <vagrant@precise32.(none)>
2016-02-18 17:59:54 +0000

    grid experiments

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,20 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+  var random_num = Math.random();
+  console.log(random_num);
+
+  if (random_num >= 0) {
+    projects['grid'] = false;
+    res.render('index', projects);
+  } else {
+    res.redirect('/grid');
+  }
   	res.render('index', projects);
     projects["grid"] = true;
 };
 
 exports.viewGrid = function(req, res){
     res.render('index', projects);
-    projects["grid"] = false;
+    projects["grid"] = false
 }
